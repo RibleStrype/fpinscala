e7030c909c1ca2f53ccf8b6f435a2dbad8fd6b91
Eriks Harcevs <eharcevs@evolutiongaming.com>
2019-11-19 11:22:54 +0200

    exercise 3.5

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -67,7 +67,11 @@ object List { // `List` companion object. Contains functions for creating and wo
       case Cons(_, tail) => drop(tail, n - 1)
     }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(head, tail)
+      if f(head) => dropWhile(tail, f)
+    case _ => l
+  }
 
   def init[A](l: List[A]): List[A] = ???
 

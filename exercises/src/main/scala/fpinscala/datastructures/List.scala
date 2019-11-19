42d7326b97554d6685fe9fbdaabb4273d1cff368
Eriks Harcevs <eharcevs@evolutiongaming.com>
2019-11-19 11:19:30 +0200

    exercise 3.4

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -60,7 +60,12 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Nil           => sys.error("setHead on empty list")
   }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = 
+    if (n <= 0) l
+    else l match {
+      case Nil           => Nil
+      case Cons(_, tail) => drop(tail, n - 1)
+    }
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
 

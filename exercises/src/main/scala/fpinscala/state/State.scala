a0026bf404326276a1f5c18e0b5e39ef19a1497f
Eriks Harcevs <eharcevs@evolutiongaming.com>
2019-12-16 15:34:24 +0200

    exercise 6.7

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -60,17 +60,19 @@ object RNG {
   }
 
   def ints(count: Int)(rng: RNG): (List[Int], RNG) =
-    List.fill(count)(()).foldLeft((List.empty[Int], rng)) {
-      case ((ns, rng), _) =>
-        val (n, rng2) = rng.nextInt
-        (n :: ns, rng2)
-    }
+    sequence(List.fill(count)(int))(rng)
 
   val _double: Rand[Double] = map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng2) = ra(rng)
+      val (b, rng3) = rb(rng2)
+      (f(a, b), rng3)
+    }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit(List.empty[A]))(map2(_, _)(_ :: _))
 
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
 }

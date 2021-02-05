a18c0ccd5cba94e107d21bbd0760b3651b9cbca0
Eriks Harcevs <eharcevs@evolutiongaming.com>
2021-02-05 16:38:34 +0200

    exercise 11.3

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -36,9 +36,13 @@ trait Monad[M[_]] extends Functor[M] {
   def map2[A,B,C](ma: M[A], mb: M[B])(f: (A, B) => C): M[C] =
     flatMap(ma)(a => map(mb)(b => f(a, b)))
 
-  def sequence[A](lma: List[M[A]]): M[List[A]] = ???
+  def sequence[A](lma: List[M[A]]): M[List[A]] =
+    traverse(lma)(identity)
 
-  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] = ???
+  def traverse[A,B](la: List[A])(f: A => M[B]): M[List[B]] =
+    la.foldRight(unit(List.empty[B])) { (a, mbs) =>
+      map2(f(a), mbs)(_ :: _)
+    }
 
   def replicateM[A](n: Int, ma: M[A]): M[List[A]] = ???
 
